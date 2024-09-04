c734a3e77e028b990b1de107f21efbbeb3bb4e83
pwwang <pwwang@pwwang.com>
2024-09-04 08:49:50 -0700

    feat(BarPlot): support count for y and flipping axes

--- R/barplot.R
+++ R/barplot.R
@@ -3,16 +3,20 @@
 #' @description Create a bar plot without groups.
 #'
 #' @inheritParams common_args
+#' @param y A character vector specifying the column as the y axis of the plot.
+#'   Default is NULL, meaning the y axis is the count of the data.
 #' @param fill_by_x A logical value indicating whether to fill the bars by the x-axis values.
 #'   If FALSE, the bars will be filled a single color (the first color in the palette).
 #' @param width A numeric value specifying the width of the bars.
+#' @param flip A logical value indicating whether to flip the x and y axes.
 #' @return A ggplot object.
 #' @keywords internal
 #' @importFrom rlang sym %||%
+#' @importFrom dplyr %>% group_by summarise n
 #' @importFrom gglogger ggplot
-#' @importFrom ggplot2 aes geom_bar scale_fill_manual labs scale_x_discrete scale_y_continuous element_line waiver
+#' @importFrom ggplot2 aes geom_bar scale_fill_manual labs scale_x_discrete scale_y_continuous element_line waiver coord_flip
 BarPlotSingle <- function(
-    data, x, y,
+    data, x, y = NULL, flip = FALSE,
     theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL,
     alpha = 1, x_text_angle = 0, aspect.ratio = 1,
     legend.position = "right", legend.direction = "vertical",
@@ -25,6 +29,10 @@ BarPlotSingle <- function(
 
     x <- check_columns(data, x, force_factor = TRUE)
     y <- check_columns(data, y)
+    if (is.null(y)) {
+        data <- data %>% group_by(!!sym(x)) %>% summarise(.y = n(), .groups = "drop")
+        y <- ".y"
+    }
 
     if (isTRUE(fill_by_x)) {
         p <- ggplot(data, aes(x = !!sym(x), y = !!sym(y), fill = !!sym(x)))
@@ -54,6 +62,10 @@ BarPlotSingle <- function(
             axis.text.x = element_text(angle = x_text_angle, hjust = just$h, vjust = just$v)
         )
 
+    if (isTRUE(flip)) {
+        p <- p + coord_flip()
+    }
+
     height <- 4.5
     width <- .5 + length(levels(data[[x]])) * .8
     if (guide == "legend") {
@@ -65,8 +77,14 @@ BarPlotSingle <- function(
             width <- width + 2
         }
     }
-    attr(p, "height") <- height
-    attr(p, "width") <- width
+
+    if (isTRUE(flip)) {
+        attr(p, "height") <- width
+        attr(p, "width") <- height
+    } else {
+        attr(p, "height") <- height
+        attr(p, "width") <- width
+    }
     p
 }
 
@@ -75,6 +93,8 @@ BarPlotSingle <- function(
 #' @description Create a bar plot with groups.
 #'
 #' @inheritParams common_args
+#' @param y A character vector specifying the column as the y axis of the plot.
+#'  Default is NULL, meaning the y axis is the count of the data.
 #' @param position A character string indicating the position of the bars.
 #'  If "auto", the position will be "stack" if group_by has more than 5 levels, otherwise "dodge".
 #'  "fill" is also a valid option. Only works when group_by is not NULL.
@@ -83,13 +103,15 @@ BarPlotSingle <- function(
 #' @param bg_palette A character string indicating the palette to use for the background.
 #' @param bg_palcolor A character string indicating the color to use for the background.
 #' @param bg_alpha A numeric value indicating the alpha of the background.
+#' @param flip A logical value indicating whether to flip the x and y axes.
 #' @return A ggplot object.
 #' @keywords internal
 #' @importFrom rlang sym %||%
+#' @importFrom dplyr %>% group_by summarise n
 #' @importFrom gglogger ggplot
-#' @importFrom ggplot2 aes geom_bar scale_fill_manual labs position_dodge2
+#' @importFrom ggplot2 aes geom_bar scale_fill_manual labs position_dodge2 coord_flip
 BarPlotGrouped <- function(
-    data, x, y, group_by, group_by_sep = "_",
+    data, x, y = NULL, flip = FALSE, group_by, group_by_sep = "_",
     theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL,
     add_bg = FALSE, bg_palette = "stripe", bg_palcolor = NULL, bg_alpha = 0.2,
     alpha = 1, x_text_angle = 0, aspect.ratio = 1,
@@ -102,9 +124,15 @@ BarPlotGrouped <- function(
     if (inherits(expand, "waiver")) expand <- c(bottom = 0)
     expand <- norm_expansion(expand, x_type = "discrete", y_type = "continuous")
 
+    group_by <- check_columns(data, group_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = group_by_sep)
     x <- check_columns(data, x, force_factor = TRUE)
     y <- check_columns(data, y)
-    group_by <- check_columns(data, group_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = group_by_sep)
+    if (is.null(y)) {
+        data <- data %>%
+            group_by(!!!syms(unique(c(x, group_by, facet_by)))) %>%
+            summarise(.y = n(), .groups = "drop")
+        y <- ".y"
+    }
 
     p <- ggplot(data, aes(x = !!sym(x), y = !!sym(y), fill = !!sym(group_by)))
     if (isTRUE(add_bg)) {
@@ -139,6 +167,10 @@ BarPlotGrouped <- function(
             axis.text.x = element_text(angle = x_text_angle, hjust = just$h, vjust = just$v)
         )
 
+    if (isTRUE(flip)) {
+        p <- p + coord_flip()
+    }
+
     height <- 4.5
     width <- .5 + length(levels(data[[x]])) * length(unique(data[[group_by]])) * .5
     if (legend.position %in% c("right", "left")) {
@@ -148,20 +180,29 @@ BarPlotGrouped <- function(
     } else {
         width <- width + 2
     }
-    attr(p, "height") <- height
-    attr(p, "width") <- width
+
+    if (isTRUE(flip)) {
+        attr(p, "height") <- width
+        attr(p, "width") <- height
+    } else {
+        attr(p, "height") <- height
+        attr(p, "width") <- width
+    }
     p
 }
 
 #' Atomic bar plot
 #' @description Create a bar plot with or without groups. This function does not handle splitting but only facetting.
 #' @inheritParams common_args
+#' @param y A character vector specifying the column as the y axis of the plot.
+#'  Default is NULL, meaning the y axis is the count of the data.
 #' @param fill_by_x_if_no_group A logical value indicating whether to fill the bars by the x-axis values if there is no group_by.
+#' @param flip A logical value indicating whether to flip the x and y axes.
 #' @return A ggplot object.
 #' @importFrom ggplot2 waiver
 #' @keywords internal
 BarPlotAtomic <- function(
-    data, x, y, group_by = NULL, fill_by_x_if_no_group = TRUE,
+    data, x, y = NULL, flip = FALSE, group_by = NULL, fill_by_x_if_no_group = TRUE,
     theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL,
     alpha = 1, x_text_angle = 0, aspect.ratio = 1,
     position = "auto", position_dodge_preserve = "total",
@@ -172,7 +213,7 @@ BarPlotAtomic <- function(
 ) {
     if (is.null(group_by)) {
         p <- BarPlotSingle(
-            data, x, y, facet_by = facet_by,
+            data, x, y, facet_by = facet_by, flip = flip,
             theme = theme, theme_args = theme_args, palette = palette, palcolor = palcolor,
             alpha = alpha, x_text_angle = x_text_angle, aspect.ratio = aspect.ratio,
             legend.position = legend.position, legend.direction = legend.direction,
@@ -181,7 +222,7 @@ BarPlotAtomic <- function(
         )
     } else {
         p <- BarPlotGrouped(
-            data, x, y, group_by, facet_by = facet_by,
+            data, x, y, group_by, facet_by = facet_by, flip = flip,
             theme = theme, theme_args = theme_args, palette = palette, palcolor = palcolor,
             alpha = alpha, x_text_angle = x_text_angle, aspect.ratio = aspect.ratio,
             position = position, position_dodge_preserve = position_dodge_preserve,
@@ -219,8 +260,12 @@ BarPlotAtomic <- function(
 #'         facet_by = "facet", position = "dodge", facet_ncol = 1)
 #' BarPlot(data, x = "group", y = "y", group_by = "x",
 #'         position = "dodge", add_bg = TRUE, bg_palette = "Spectral")
+#' # use the count
+#' BarPlot(data, x = "group", ylab = "count")
+#' # flip the plot
+#' BarPlot(data, x = "group", flip = TRUE, ylab = "count")
 BarPlot <- function(
-    data, x, y, fill_by_x_if_no_group = TRUE,
+    data, x, y = NULL, flip = FALSE, fill_by_x_if_no_group = TRUE,
     group_by = NULL, group_by_sep = "_", split_by = NULL, split_by_sep = "_",
     facet_by = NULL, facet_scales = "fixed", facet_ncol = NULL, facet_nrow = NULL, facet_byrow = TRUE,
     add_bg = FALSE, bg_palette = "stripe", bg_palcolor = NULL, bg_alpha = 0.2,
@@ -233,9 +278,6 @@ BarPlot <- function(
 ) {
     validate_common_args(seed, facet_by = facet_by)
 
-    x <- check_columns(data, x, force_factor = TRUE)
-    y <- check_columns(data, y)
-    group_by <- check_columns(data, group_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = group_by_sep)
     split_by <- check_columns(data, split_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = split_by_sep)
 
     if (!is.null(split_by)) {
@@ -248,7 +290,7 @@ BarPlot <- function(
 
     plots <- lapply(
         datas, BarPlotAtomic,
-        x = x, y = y, group_by = group_by, fill_by_x_if_no_group = fill_by_x_if_no_group,
+        x = x, y = y, flip = flip, group_by = group_by, fill_by_x_if_no_group = fill_by_x_if_no_group,
         theme = theme, theme_args = theme_args, palette = palette, palcolor = palcolor,
         add_bg = add_bg, bg_palette = bg_palette, bg_palcolor = bg_palcolor, bg_alpha = bg_alpha,
         alpha = alpha, x_text_angle = x_text_angle, aspect.ratio = aspect.ratio,
