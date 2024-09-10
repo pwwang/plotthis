ec071738d54acc3b7bdd020acfefc200edaa9231
pwwang <pwwang@pwwang.com>
2024-09-10 15:24:26 -0700

    feat: Add WordCloudPlot function for creating word clouds without data splitting

---
+++ R/wordcloudplot.R
@@ -0,0 +1,219 @@
+#' Word cloud without data splitting
+#'
+#' @inheritParams common_args
+#' @param word_by A character string of the column name to use as the word.
+#'  A character column is expected.
+#' @param sentence_by A character string of the column name to split the sentence.
+#'  A character column is expected.
+#'  Either `word_by` or `sentence_by` should be specified.
+#' @param count_by A character string of the column name for the count of the word/sentence.
+#'  A numeric column is expected.
+#'  If NULL, the count of the word/sentence will be used.
+#' @param score_by A character string of the column name for the score of the word/sentence.
+#'  A numeric column is expected, used for the color of the word cloud.
+#'  If NULL, the score will be set to 1.
+#' @param count_name A character string to name the legend of count.
+#' @param score_name A character string to name the legend of score.
+#' @param words_excluded A character vector of words to exclude from the word cloud.
+#' @param score_agg A function to aggregate the scores. Default is `mean`.
+#' @param minchar A numeric value specifying the minimum number of characters for the word.
+#' @param word_size A numeric vector specifying the range of the word size.
+#' @param top_words A numeric value specifying the number of top words to show.
+#' @return A ggplot object
+#' @keywords internal
+#' @importFrom tidyr unnest
+#' @importFrom dplyr reframe slice_max distinct mutate group_by summarise n
+WordCloudPlotAtomic <- function(
+    data, word_by = NULL, sentence_by = NULL, count_by = NULL, score_by = NULL,
+    count_name = NULL, score_name = NULL,
+    words_excluded = plotthis::words_excluded, score_agg = mean, minchar = 2,
+    word_size = c(2, 8), top_words = 100,
+    facet_by = NULL, facet_scales = "fixed", facet_ncol = NULL, facet_nrow = NULL, facet_byrow = TRUE,
+    theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL, alpha = 1, palreverse = FALSE,
+    aspect.ratio = 1, legend.position = "right", legend.direction = "vertical",
+    title = NULL, subtitle = NULL, seed = 8525, ...
+) {
+    if (sum(is.null(word_by), is.null(sentence_by)) != 1) {
+        stop("Either 'word_by' or 'sentence_by' should be specified.")
+    }
+    if (!is.null(sentence_by) && !is.null(count_by)) {
+        stop("Cannot specify 'count_by' when 'sentence_by' is specified.")
+    }
+    facet_by <- check_columns(data, facet_by, force_factor = TRUE, allow_multi = TRUE)
+    count_by <- check_columns(data, count_by)
+    score_by <- check_columns(data, score_by)
+    if (is.null(score_by)) {
+        data$.score <- 1
+        score_by <- ".score"
+    }
+    if (is.null(count_by)) {
+        data$.count <- 1
+        count_by <- ".count"
+    }
+
+    if (!is.null(sentence_by)) {
+        sentence_by <- check_columns(data, sentence_by)
+        data <- data %>%
+            mutate(word = strsplit(tolower(gsub("[[:punct:]]", "", as.character(!!sym(sentence_by)))), "\\b\\s+\\b")) %>%
+            unnest(cols = "word")
+
+        if (length(facet_by) == 1) {
+            data <- data %>%
+                group_by(word, !!!syms(facet_by)) %>%
+                reframe(
+                    word = word,
+                    !!sym(facet_by) := !!sym(facet_by),
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        } else if (length(facet_by) == 2) {
+            data <- data %>%
+                group_by(word, !!!syms(facet_by)) %>%
+                reframe(
+                    word = word,
+                    !!sym(facet_by[1]) := !!sym(facet_by[1]),
+                    !!sym(facet_by[2]) := !!sym(facet_by[2]),
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        } else {
+            data <- data %>%
+                group_by(word) %>%
+                reframe(
+                    word = word,
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        }
+    } else {
+        word_by <- check_columns(data, word_by)
+        data <- data %>% unnest(cols = word_by)
+
+        if (length(facet_by) == 1) {
+            data <- data %>%
+                group_by(!!!syms(unique(c(word_by, facet_by)))) %>%
+                reframe(
+                    word = !!sym(word_by),
+                    !!sym(facet_by) := !!sym(facet_by),
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        } else if (length(facet_by) == 2) {
+            data <- data %>%
+                group_by(!!!syms(unique(c(word_by, facet_by)))) %>%
+                reframe(
+                    word = !!sym(word_by),
+                    !!sym(facet_by[1]) := !!sym(facet_by[1]),
+                    !!sym(facet_by[2]) := !!sym(facet_by[2]),
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        } else {
+            data <- data %>%
+                group_by(!!sym(word_by)) %>%
+                reframe(
+                    word = !!sym(word_by),
+                    count = sum(!!sym(count_by)),
+                    score = score_agg(!!sym(score_by)),
+                    .groups = "keep"
+                )
+        }
+    }
+
+    data <- data %>%
+        filter(!grepl(pattern = "\\[.*\\]", x = word)) %>%
+        filter(nchar(word) >= minchar) %>%
+        filter(!tolower(word) %in% tolower(words_excluded)) %>%
+        distinct() %>%
+        slice_max(order_by = score, n = top_words) %>%
+        mutate(angle = 90 * sample(c(0, 1), n(), replace = TRUE, prob = c(60, 40))) %>%
+        as.data.frame()
+
+    colors <- palette_this(data$score, type = "continuous", palette = palette, palcolor = palcolor, matched = FALSE, reverse = palreverse)
+    colors_value <- seq(min(data$score, na.rm = TRUE), quantile(data$score, 0.99, na.rm = TRUE) + 0.001, length.out = 100)
+    p <- ggplot(data, aes(label = word, size = count, color = score, angle = angle)) +
+        ggwordcloud::geom_text_wordcloud(rm_outside = TRUE, eccentricity = 1, shape = "square", show.legend = TRUE, grid_margin = 3) +
+        scale_color_gradientn(
+            name = score_name %||% "Score", colours = colors, values = scales::rescale(colors_value),
+            guide = guide_colorbar(frame.colour = "black", ticks.colour = "black", title.hjust = 0)
+        ) +
+        scale_size(name = count_name %||% "Count", range = word_size, breaks = ceiling(seq(min(data$count, na.rm = TRUE), max(data$count, na.rm = TRUE), length.out = 3))) +
+        guides(size = guide_legend(override.aes = list(colour = "black", label = "G"), order = 1)) +
+        labs(title = title, subtitle = subtitle) +
+        coord_flip() +
+        do.call(theme, theme_args) +
+        ggplot2::theme(
+            aspect.ratio = aspect.ratio,
+            legend.position = legend.position,
+            legend.direction = legend.direction
+        )
+
+    height <- width <- 4.5
+    if (legend.position %in% c("right", "left")) {
+        width <- width + 1
+    } else if (legend.direction == "horizontal") {
+        height <- height + 1
+    } else {
+        height <- height + 2
+    }
+
+    attr(p, "height") <- height
+    attr(p, "width") <- width
+
+    facet_plot(p, facet_by, facet_scales, facet_nrow, facet_ncol, facet_byrow)
+}
+
+#' Word Cloud Plot
+#'
+#' @inheritParams common_args
+#' @inheritParams WordCloudPlotAtomic
+#' @return A ggplot object or wrap_plots object or a list of ggplot objects
+#' @export
+#' @examples
+#' data <- data.frame(
+#'    word = c("apple", "banana", "cherry", "date", "elderberry"),
+#'    count = c(10, 20, 30, 40, 50),
+#'    score = c(1, 2, 3, 4, 5)
+#' )
+#' WordCloudPlot(data, word_by = "word", count_by = "count", score_by = "score")
+WordCloudPlot <- function(
+    data, word_by = NULL, sentence_by = NULL, count_by = NULL, score_by = NULL,
+    count_name = NULL, score_name = NULL, split_by = NULL, split_by_sep = "_",
+    words_excluded = plotthis::words_excluded, score_agg = mean, minchar = 2,
+    word_size = c(2, 8), top_words = 100,
+    facet_by = NULL, facet_scales = "fixed", facet_ncol = NULL, facet_nrow = NULL, facet_byrow = TRUE,
+    theme = "theme_this", theme_args = list(), palette = "Spectral", palcolor = NULL, alpha = 1, palreverse = FALSE,
+    aspect.ratio = 1, legend.position = "right", legend.direction = "vertical",
+    title = NULL, subtitle = NULL, seed = 8525,
+    combine = TRUE, nrow = NULL, ncol = NULL, byrow = TRUE, ...
+    ) {
+    validate_common_args(seed, facet_by = facet_by)
+
+    split_by <- check_columns(data, split_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = split_by_sep)
+
+    if (!is.null(split_by)) {
+        datas <- split(data, data[[split_by]])
+        # keep the order of levels
+        datas <- datas[levels(data[[split_by]])]
+    } else {
+        datas <- list(data)
+    }
+
+    plots <- lapply(
+        datas, WordCloudPlotAtomic,
+        word_by = word_by, sentence_by = sentence_by, count_by = count_by, score_by = score_by,
+        count_name = count_name, score_name = score_name, words_excluded = words_excluded, score_agg = score_agg, minchar = minchar,
+        word_size = word_size, top_words = top_words,
+        facet_by = facet_by, facet_scales = facet_scales, facet_ncol = facet_ncol, facet_nrow = facet_nrow, facet_byrow = facet_byrow,
+        theme = theme, theme_args = theme_args, palette = palette, palcolor = palcolor, alpha = alpha, palreverse = palreverse,
+        aspect.ratio = aspect.ratio, legend.position = legend.position, legend.direction = legend.direction,
+        title = title, subtitle = subtitle, seed = seed, ...
+    )
+
+    combine_plots(plots, combine = combine, nrow = nrow, ncol = ncol, byrow = byrow)
+}
