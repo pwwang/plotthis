dc9fe4c53a06357f4b930d870913a42dd298dc55
pwwang <pwwang@pwwang.com>
2024-09-29 20:47:49 -0700

    change: rename fill_by to group_by in AreaPlot function

--- R/areaplot.R
+++ R/areaplot.R
@@ -8,12 +8,12 @@
 #'  A numeric column is expected.
 #'  If NULL, the count of the x-axis column will be used.
 #' @param scale_y A logical value to scale the y-axis by the total number in each x-axis group.
-#' @param fill_by A character vector of column names to fill the area plot by.
+#' @param group_by A character vector of column names to fill the area plot by.
 #'  If NULL, the plot will be filled by the first color of the palette.
 #'  If multiple columns are provided, the columns will be concatenated with
-#'  `fill_by_sep` and used as the fill column.
-#' @param fill_by_sep A character string to separate the columns in `fill_by`.
-#' @param fill_name A character string to name the legend of fill.
+#'  `group_by_sep` and used as the fill column.
+#' @param group_by_sep A character string to separate the columns in `group_by`.
+#' @param group_name A character string to name the legend of fill.
 #' @keywords internal
 #' @return A ggplot object
 #' @importFrom rlang syms :=
@@ -21,7 +21,7 @@
 #' @importFrom ggplot2 geom_area scale_x_discrete scale_y_continuous scale_fill_manual
 #' @importFrom ggplot2 labs theme element_line element_text position_stack waiver
 AreaPlotAtomic <- function(
-    data, x, y = NULL, x_sep = "_", fill_by = NULL, fill_by_sep = "_", fill_name = NULL, scale_y = FALSE,
+    data, x, y = NULL, x_sep = "_", group_by = NULL, group_by_sep = "_", group_name = NULL, scale_y = FALSE,
     theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL, alpha = 1,
     facet_by = NULL, facet_scales = "fixed", facet_ncol = NULL, facet_nrow = NULL, facet_byrow = TRUE,
     x_text_angle = 0, aspect.ratio = 1, legend.position = waiver(), legend.direction = "vertical",
@@ -29,12 +29,12 @@ AreaPlotAtomic <- function(
 ) {
     x <- check_columns(data, x, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = x_sep)
     y <- check_columns(data, y)
-    fill_by <- check_columns(data, fill_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = fill_by_sep)
+    group_by <- check_columns(data, group_by, force_factor = TRUE, allow_multi = TRUE, concat_multi = TRUE, concat_sep = group_by_sep)
 
     if (is.null(y)) {
         y <- ".count"
         data <- data %>%
-            group_by(!!!syms(unique(c(x, fill_by, facet_by)))) %>%
+            group_by(!!!syms(unique(c(x, group_by, facet_by)))) %>%
             summarise(.count = n(), .groups = "drop")
     }
 
@@ -44,18 +44,18 @@ AreaPlotAtomic <- function(
             mutate(!!sym(y) := !!sym(y) / sum(!!sym(y)))
     }
 
-    if (is.null(fill_by)) {
+    if (is.null(group_by)) {
         data$.fill <- factor("")
-        fill_by <- ".fill"
+        group_by <- ".fill"
         legend.position <- ifelse(inherits(legend.position, "waiver"), "none", "right")
     } else {
-        data[[fill_by]] <- droplevels(data[[fill_by]])
-        # fill up some missing fill_by values for each x, and fill it with 0 for y
-        fill_levels <- levels(data[[fill_by]])
+        data[[group_by]] <- droplevels(data[[group_by]])
+        # fill up some missing group_by values for each x, and fill it with 0 for y
+        fill_levels <- levels(data[[group_by]])
         complete_fill <- list(0)
         names(complete_fill) <- y
         data <- data %>%
-            group_by(!!!syms(unique(c(fill_by, facet_by)))) %>%
+            group_by(!!!syms(unique(c(group_by, facet_by)))) %>%
             complete(!!sym(x), fill = complete_fill) %>%
             ungroup()
         legend.position <- ifelse(inherits(legend.position, "waiver"), "right", legend.position)
@@ -63,13 +63,13 @@ AreaPlotAtomic <- function(
     xs <- if (isTRUE(keep_empty)) levels(data[[x]]) else levels(droplevels(data[[x]]))
 
     just <- calc_just(x_text_angle)
-    p <- ggplot(data, aes(x = as.numeric(!!sym(x)), y = !!sym(y), fill = !!sym(fill_by))) +
+    p <- ggplot(data, aes(x = as.numeric(!!sym(x)), y = !!sym(y), fill = !!sym(group_by))) +
         geom_area(alpha = alpha, color = "grey50", position = position_stack(vjust = 0.5)) +
         scale_x_discrete(drop = !keep_empty, expand = c(0, 0), breaks = xs, limits = xs) +
         scale_y_continuous(expand = c(0, 0), labels = if (isFALSE(scale_y)) scales::number else scales::percent) +
         scale_fill_manual(
-            name = fill_name %||% fill_by,
-            values = palette_this(levels(data[[fill_by]]), palette = palette, palcolor = palcolor)
+            name = group_name %||% group_by,
+            values = palette_this(levels(data[[group_by]]), palette = palette, palcolor = palcolor)
         ) +
         labs(title = title, subtitle = subtitle, x = xlab %||% x, y = ylab %||% y) +
         do.call(theme, theme_args) +
@@ -113,15 +113,15 @@ AreaPlotAtomic <- function(
 #' data <- data.frame(
 #'     x = rep(c("A", "B", "C", "D"), 2),
 #'     y = c(1, 3, 6, 4, 2, 5, 7, 8),
-#'     fill = rep(c("F1", "F2"), each = 4)
+#'     group = rep(c("F1", "F2"), each = 4)
 #' )
-#' AreaPlot(data, x = "x", y = "y", fill_by = "fill")
-#' AreaPlot(data, x = "x", y = "y", fill_by = "fill",
+#' AreaPlot(data, x = "x", y = "y", group_by = "group")
+#' AreaPlot(data, x = "x", y = "y", group_by = "group",
 #'          scale_y = TRUE)
-#' AreaPlot(data, x = "x", y = "y", split_by = "fill")
+#' AreaPlot(data, x = "x", y = "y", split_by = "group")
 AreaPlot <- function(
     data, x, y = NULL, x_sep = "_", split_by = NULL, split_by_sep = "_",
-    fill_by = NULL, fill_by_sep = "_", fill_name = NULL, scale_y = FALSE,
+    group_by = NULL, group_by_sep = "_", group_name = NULL, scale_y = FALSE,
     theme = "theme_this", theme_args = list(), palette = "Paired", palcolor = NULL, alpha = 1,
     facet_by = NULL, facet_scales = "fixed", facet_ncol = NULL, facet_nrow = NULL, facet_byrow = TRUE,
     x_text_angle = 0, aspect.ratio = 1, legend.position = waiver(), legend.direction = "vertical",
@@ -150,7 +150,7 @@ AreaPlot <- function(
             }
             AreaPlotAtomic(
                 datas[[nm]],
-                x = x, y = y, x_sep = x_sep, fill_by = fill_by, fill_by_sep = fill_by_sep, fill_name = fill_name, scale_y = scale_y,
+                x = x, y = y, x_sep = x_sep, group_by = group_by, group_by_sep = group_by_sep, group_name = group_name, scale_y = scale_y,
                 theme = theme, theme_args = theme_args, palette = palette, palcolor = palcolor, alpha = alpha,
                 facet_by = facet_by, facet_scales = facet_scales, facet_ncol = facet_ncol, facet_nrow = facet_nrow, facet_byrow = facet_byrow,
                 x_text_angle = x_text_angle, aspect.ratio = aspect.ratio, legend.position = legend.position, legend.direction = legend.direction,
